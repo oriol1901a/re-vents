ef18441a079300989a4045aef4068607909dff3d
oriol1901 <boomjack1901@gmail.com>
2018-11-20 01:55:45 +0100

    Basic layout + NavBar layout

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import React, { Component, Fragment } from 'react';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Re-vents</h1>
-        <button className='ui icon button'>
-          <i className='smile icon' />
-          CSS Button
-        </button>
-        <Button icon='smile icon' content='React button' />
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
